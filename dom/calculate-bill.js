f4df3f0eef1025f2328e3c9e25fdea6c4b04c858
Busiswa <onakomini@gmail.com>
2020-07-14 09:01:40 +0200

    calculate-dom

--- dom/calculate-bill.js
+++ dom/calculate-bill.js
@@ -1,1 +1,34 @@
-const roundedBillTotal = totalPhoneBill();
+//get a reference to the calculate button
+const calculateBtn = document.querySelector(".calculateBtn")
+//get a reference to the billTotal element
+const billTotalElement = document.querySelector(".billTotal")
+//get a reference to the billString
+const billStringField = document.querySelector(".billString")
+//get a reference to total span
+const billTotalSpanElement = document.querySelector(".total")
+//create the function that will be called when the calculate button is pressed
+//  * this function should read the string value entered - split it on a comma.
+//  * loop over all the entries in the the resulting list
+//  * check if it is a call or an sms and add the right amount to the overall total
+//  * once done looping over all the entries - display the total onto the screen in the billTotal element
+function styleTotalColor(roundedBillTotal){
+const currentTotal =Number(roundedBillTotal)
+billTotalSpanElement.classList.remove("danger")
+billTotalSpanElement.classList.remove("warning")
+
+if (currentTotal >= 30){
+    billTotalSpanElement.classList.add("danger")
+}
+else if(currentTotal >= 20 && currentTotal < 30){
+    billTotalSpanElement.classList.add("warning")
+}
+}
+totalPhoneBill("call,sms")
+//link the function to a click event on the calculate button
+function calculateBtnClicked(){
+    var billString =billStringField.value
+    const roundedBillTotal=totalPhoneBill(billString)
+    billTotalElement.innerHTML=roundedBillTotal;
+    styleTotalColor(roundedBillTotal)
+}
+calculateBtn.addEventListener("click", calculateBtnClicked);
